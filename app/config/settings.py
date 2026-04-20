b36f0d7c4912a8c686e139a6c23bbd62727b8ff2
Andrii Rohovenko <andrii.rohovenko@Andriis-MacBook-Pro.local>
2026-04-21 00:02:53 +0300

    updated getting correct env vars in settings module

--- app/config/settings.py
+++ app/config/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-lcmc(gw-p=qp^m7v%sl-m#w6trlbz3ru#k@--6eu*as!rbe4jy"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-demo-key")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host.strip() for host in os.getenv("ALLOWED_HOSTS", "").split(",") if host.strip()
+]
 
 
 # Application definition
