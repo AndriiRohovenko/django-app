65b9fc15039056bdd0dbf8705caf914f9cc6ed2e
Andrii Rohovenko <andrii.rohovenko@Andriis-MacBook-Pro.local>
2026-04-21 00:11:28 +0300

    add homepage route with welcome text to check implemented CI/CD

--- app/config/urls.py
+++ app/config/urls.py
@@ -17,7 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Django app is running")
+
 
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
 ]
